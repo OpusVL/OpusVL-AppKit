ba3e6395c25385aa847c67dd45fba06181734c43
Alastair McGowan-Douglas <alastair.mcgowan@opusvl.com>
2015-12-14 15:29:16 +0000

    Bumped version number

--- lib/OpusVL/AppKit.pm
+++ lib/OpusVL/AppKit.pm
@@ -78,7 +78,7 @@ This software is licensed according to the "IP Assignment Schedule" provided wit
 use strict;
 use warnings;
 use OpusVL::AppKit::Builder;
-our $VERSION = '2.16';
+our $VERSION = '2.17';
 
 ##################################################################################################################################
 # main #
